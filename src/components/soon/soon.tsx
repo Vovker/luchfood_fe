872e11368d2da8c926d2e141502073d42a1c6a4e
Aliaksei Sushkevich <aliaksei.sushkevich@sqrd.tech>
2022-07-26 18:50:34 +0300

    added soon

---
+++ src/components/soon/soon.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import soon from '../../assets/soon/soon.png'
+import {SoonImg} from './styled'
+
+export const Soon = () => {
+  return (
+    <div>
+      <SoonImg src={soon} alt='скоро'/>
+    </div>
+  )
+}
---
+++ src/components/soon/styled.ts
@@ -0,0 +1,8 @@
+import styled from "styled-components";
+
+const SoonImg = styled.img`
+  width: 980px;
+  height: 552px;
+`
+
+export {SoonImg}
