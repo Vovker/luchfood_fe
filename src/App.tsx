7dd1bad356d65f226685f81c58521f5235dedf34
Aliaksei Sushkevich <aliaksei.sushkevich@sqrd.tech>
2022-07-26 18:54:24 +0300

    added soon

--- src/App.tsx
+++ src/App.tsx
@@ -17,8 +17,7 @@ function App() {
       <Router>
         <Routes>
           <Route path="/" element={<Layout/>}>
-            <Route index element={<Home/>}/>
-            <Route path="soon" element={<Soon/>}/>
+            <Route index element={<Soon/>}/>
           </Route>
         </Routes>
       </Router>
