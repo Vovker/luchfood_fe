105379f397ae2f9fd7dd9fb8a698c789342ac660
Aliaksei Sushkevich <aliaksei.sushkevich@sqrd.tech>
2022-07-31 15:42:25 +0300

    mobile layout NewsItemPage

--- src/components/news/newsItemPage/newsItemPage.tsx
+++ src/components/news/newsItemPage/newsItemPage.tsx
@@ -1,55 +1,78 @@
 import React from "react";
-import {InfoWrapper, ItemPageWrapper, StyledDate, Wrapper} from "../../common/styled";
-import {CardImg} from "../../common/card/styled";
+import {SocialImg, SocialLinks, StyledDate} from "../../common/styled";
 import coloredIstagramIcon from "../../../assets/social_icons/instagramColored.svg";
 import twitterIcon from "../../../assets/social_icons/twitter.svg";
 import {BackButton} from "../../common/backButton/backButton";
-import {InfoDescription, SocialImg, SocialLinks} from "../../afisha/afishaItemPage/styled";
-import {Title} from "../../common/title/title";
-import useMediaQuery from "../../../hooks/useMatchMedia";
+import {isMobile} from 'react-device-detect';
+import {NewsItemImage, NewsItemInfoDescription, NewsItemInfoWrapper, NewsItemMobileImage, NewsItemTitle, NewsItemWrapper } from "./styled";
 
 
 export const NewsItemPage: React.FC = () => {
 
-  const isDesktop = useMediaQuery('(min-width: 1073px)')
-
   return (
     <>
       <BackButton title='Все новости'/>
-      <Wrapper width={isDesktop ? 980 : 345} paddingTop={isDesktop ? 72 : 16}>
-        <ItemPageWrapper gap={isDesktop ? 55 : 26} isDesktop={isDesktop}>
-          <InfoWrapper gap={isDesktop ? 16 : 6}>
-            <Title marginBottom={14} align="center">
-              Vegan Fest- фестиваль вегетарианской кухни
-            </Title>
-            {
-              !isDesktop &&
-				<CardImg
-						 image='http://sun9-32.userapi.com/s/v1/if1/UCQ49DRyoh40eVZjeqLBnHNel6H3QZYEKD26sBErJ-xse4zEV-8ft3tsQJNGJhOpEW2WrTtZ.jpg?size=400x433&quality=96&crop=0,0,472,512&ava=1'/>
-            }
-            <StyledDate isDesktop={isDesktop}>
-              Июль 16, 2022
-            </StyledDate>
-            <InfoDescription isDesktop={isDesktop}>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam convallis placerat iaculis. Donec vitae
-              quam cursus, tempor quam non, euismod ipsum. Interdum et malesuada fames ac ante ipsum primis in faucibus.
-              Nunc viverra mi lacus, id condimentum leo fringilla vitae. Etiam convallis placerat
-            </InfoDescription>
-          </InfoWrapper>
-          <div>
-            {
-              isDesktop &&
-				<CardImg
-						 image='http://sun9-32.userapi.com/s/v1/if1/UCQ49DRyoh40eVZjeqLBnHNel6H3QZYEKD26sBErJ-xse4zEV-8ft3tsQJNGJhOpEW2WrTtZ.jpg?size=400x433&quality=96&crop=0,0,472,512&ava=1'/>
-            }
-            <SocialLinks isDesktop={isDesktop}>
-              Поделиться:
-              <SocialImg src={coloredIstagramIcon}/>
-              <SocialImg src={twitterIcon}/>
-            </SocialLinks>
-          </div>
-        </ItemPageWrapper>
-      </Wrapper>
+      {
+        !isMobile
+          ? <DesktopLayout/>
+          : <MobileLayout/>
+      }
     </>
+  )
+}
+
+const DesktopLayout = () => {
+  return (
+    <NewsItemWrapper>
+      <NewsItemInfoWrapper>
+        <NewsItemTitle>
+          Vegan Fest
+        </NewsItemTitle>
+        <StyledDate>
+          Июль 16, 2022
+        </StyledDate>
+        <NewsItemInfoDescription>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam convallis placerat iaculis. Donec vitae
+          quam cursus, tempor quam non, euismod ipsum. Interdum et malesuada fames ac ante ipsum primis in faucibus.
+          Nunc viverra mi lacus, id condimentum leo fringilla vitae. Etiam convallis placerat
+        </NewsItemInfoDescription>
+      </NewsItemInfoWrapper>
+      <div>
+        <NewsItemImage
+          image='http://sun9-32.userapi.com/s/v1/if1/UCQ49DRyoh40eVZjeqLBnHNel6H3QZYEKD26sBErJ-xse4zEV-8ft3tsQJNGJhOpEW2WrTtZ.jpg?size=400x433&quality=96&crop=0,0,472,512&ava=1'/>
+        <SocialLinks>
+          Поделиться:
+          <SocialImg src={coloredIstagramIcon}/>
+          <SocialImg src={twitterIcon}/>
+        </SocialLinks>
+      </div>
+    </NewsItemWrapper>
+  )
+}
+
+const MobileLayout = () => {
+  return (
+    <NewsItemWrapper>
+      <NewsItemInfoWrapper>
+        <NewsItemTitle>
+          Vegan Fest- фестиваль вегетарианской кухни
+        </NewsItemTitle>
+        <NewsItemMobileImage
+          src='http://sun9-32.userapi.com/s/v1/if1/UCQ49DRyoh40eVZjeqLBnHNel6H3QZYEKD26sBErJ-xse4zEV-8ft3tsQJNGJhOpEW2WrTtZ.jpg?size=400x433&quality=96&crop=0,0,472,512&ava=1'/>
+        <StyledDate>
+          Июль 16, 2022
+        </StyledDate>
+        <NewsItemInfoDescription>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam convallis placerat iaculis. Donec vitae
+          quam cursus, tempor quam non, euismod ipsum. Interdum et malesuada fames ac ante ipsum primis in faucibus.
+          Nunc viverra mi lacus, id condimentum leo fringilla vitae. Etiam convallis placerat
+        </NewsItemInfoDescription>
+      </NewsItemInfoWrapper>
+      <SocialLinks>
+        Поделиться:
+        <SocialImg src={coloredIstagramIcon}/>
+        <SocialImg src={twitterIcon}/>
+      </SocialLinks>
+    </NewsItemWrapper>
   )
 }
---
+++ src/components/news/newsItemPage/styled.ts
@@ -0,0 +1,72 @@
+import styled from "styled-components";
+import {CardImg} from "../../common/card/styled";
+
+const NewsItemWrapper = styled.div`
+  width: 980px;
+  padding-top: 72px;
+  margin-bottom: 60px;
+  max-width: 100%;
+  display: flex;
+  flex-direction: row;
+  gap: 55px;
+
+  @media (max-width: 480px) {
+    width: 100%;
+    margin-bottom: 40px;
+    flex-direction: column;
+    gap: 26px;
+    padding: 16px 15px 0 15px;
+  }
+`
+
+const NewsItemInfoWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 16px;
+  @media (max-width: 480px) {
+    gap: 6px;
+  }
+`
+
+const NewsItemTitle = styled.div`
+  font-size: 40px;
+  font-weight: 700;
+  line-height: 47px;
+  text-align: left;
+
+  @media (max-width: 480px) {
+    font-size: 20px;
+    line-height: 24px;
+    text-align: center;
+    margin-bottom: 20px;
+  }
+`
+
+const NewsItemImage = styled(CardImg)`
+  width: 435px;
+  height: 333px;
+  
+`
+
+const NewsItemMobileImage = styled.img`
+  width: 100%;
+`
+
+const NewsItemInfoDescription = styled.div`
+  font-size: 18px;
+  font-weight: 700;
+  line-height: 25px;
+  @media (max-width: 480px) {
+    font-size: 14px;
+    line-height: 21px;
+  }
+`
+
+export {
+  NewsItemWrapper,
+  NewsItemInfoWrapper,
+  NewsItemTitle,
+  NewsItemImage,
+  NewsItemMobileImage,
+  NewsItemInfoDescription
+}
